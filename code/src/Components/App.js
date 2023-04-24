b8ae3a7b2d73428ea533ba74d2e6544b7f33955d
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-04-24 14:22:08 +0300

    app.js fix

--- code/src/Components/App.js
+++ code/src/Components/App.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import TransactionList from './Transactionlist';
+import React, { useState, useEffect } from 'react';
+import TransactionsTable from './Transactionlist';
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8001/transactions')
+      .then(response => response.json())
+      .then(data => {
+        setTransactions(data);
+        setFilteredTransactions(data);
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  const handleSearch = event => {
+    const term = event.target.value.toLowerCase();
+    setSearchTerm(term);
+
+    const filtered = transactions.filter(
+      transaction => transaction.description.toLowerCase().includes(term)
+    );
+
+    setFilteredTransactions(filtered);
+  };
+
+  const handleAddTransaction = transaction => {
+    setTransactions([...transactions, transaction]);
+    setFilteredTransactions([...filteredTransactions, transaction]);
+  };
+
   return (
     <div>
-      <TransactionList />
+      <h1>Transactions</h1>
+      <input
+        type="text"
+        placeholder="Search by description..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <TransactionsTable
+        transactions={filteredTransactions}
+        onAddTransaction={handleAddTransaction}
+      />
     </div>
   );
 }
 
 export default App;
+
